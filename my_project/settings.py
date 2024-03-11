1938dae35f5c1a91cb6c2655b11ef2ad5c0ba5a6
Mykola Kuievda <dankraig23@gmail.com>
2024-03-11 13:47:11 +0000

    Update requirements and Procfile with gunicorn library

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w7&#m*=tl=!ve1_&&@o2=wof_q8ei@t8jr401bl_9n4h#_=#ot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
